641b1c32389e229a375a021d30fa336c30f7e42f
Mads <bockchristensen@gmail.com>
2021-01-30 10:44:27 +0100

    switching pc

--- tests/swanrest.test.js
+++ tests/swanrest.test.js
@@ -1,5 +1,6 @@
 const rest = require("../SwanRest")
-const fetch = (relativePath) => require("node-fetch")(`http://localhost:8080${relativePath}`)
+const get = (relativePath) => require("node-fetch")(`http://localhost:8080${relativePath}`)
+const post = (relativePath) => require("node-fetch")(`http://localhost:8080${relativePath}`, {method:"POST"})
 
 beforeAll(()=>{
     rest.start()
@@ -8,7 +9,7 @@ beforeAll(()=>{
 test("Can make simple request", done=>{
     rest("/a", ()=>"foo")
 
-    fetch("/a")
+    get("/a")
     .then(res=>res.text())
     .then(text=>{
         expect(text).toBe("foo")
@@ -20,13 +21,37 @@ test("Can make simple request", done=>{
 test("Can make simple request, specifically get", done=>{
     rest.get("/b", ()=>"bar")
 
-    fetch("/b")
+    get("/b")
     .then(res=>res.text())
     .then(text=>{
         expect(text).toBe("bar")
     })
     .then(done)
     .catch(err=>done(err))    
+})
+
+test("Can make simple request, specifically post", done=>{
+    rest.post("/c", ()=>"foo")
+
+    post("/c")
+    .then(res=>res.text())
+    .then(text=>{
+        expect(text).toBe("foo")
+    })
+    .then(done)
+    .catch(err=>done(err))    
+})
+
+test("Will fail if the wrong method is used for request", done=>{
+    rest.post("/d", ()=>"foo")
+
+    get("/d")
+    .then(res=>res.text())
+    .then(text=>{
+        expect(text).not.toBe("foo")
+    })
+    .then(done)
+    .catch(err=>done(err)) 
 })
 
 afterAll(()=>{
